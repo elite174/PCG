5ebacd36f99fac23dbaba1be818b5dc7dd7ec554
elite174 <lipatov.xex@gmail.com>
2016-09-27 07:50:12 +0300

    1

--- main.py
+++ main.py
@@ -6,41 +6,17 @@ from BSP import BSP
 bsp = BSP()
 digger = Digger()
 cave_gen = CellAutomata(10)
+start = time.time()
+digger.dig(5, 5)
+end = time.time()
+print('digger time = ', end - start)
 
-digger_list = []
-bsp_list = []
-cell_list = []
+start = time.time()
+cave_gen.generate_cave()
+end = time.time()
+print('cave time = ', end - start)
 
-for i in range(10):
-    bsp = BSP()
-    digger = Digger()
-    cave_gen = CellAutomata(10)
-    start = time.time()
-    digger.dig(5, 5)
-    end = time.time()
-    digger_list.append(end - start)
-    # print('digger time = ', end-start)
-
-    start = time.time()
-    cave_gen.generate_cave()
-    end = time.time()
-    cell_list.append(end - start)
-    # print('cave time = ', end-start)
-
-    start = time.time()
-    bsp.generate()
-    end = time.time()
-    bsp_list.append(end - start)
-# print('BSP time = ', end-start)
-
-sum_bsp = 0
-sum_cell = 0
-sum_digger = 0
-for i in range(10):
-    sum_bsp += bsp_list[i]
-    sum_cell += cell_list[i]
-    sum_digger += digger_list[i]
-
-print('bsp avg:', sum_bsp / 10)
-print('cell avg', sum_cell / 10)
-print('digger avg', sum_digger / 10)
+start = time.time()
+bsp.generate()
+end = time.time()
+print('BSP time = ', end - start)
